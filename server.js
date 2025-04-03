08bb442c4f9976235cdec9ed87d1e4b3a188e74f
sakthiarajapandian <sakthiapandian27@gmail.com>
2025-04-03 17:11:28 +1100

    latest

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 80;
-
 app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
+
 
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
